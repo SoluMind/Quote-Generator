d673810c838669ab144e90dc6172f0d55d230fb1
SoluMind <S.fesshaye@outlook.com>
2025-04-01 12:21:34 +1100

    initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
+// Get quotes from the API
+let apiQuote = [];
+// Show loading
+// show new quote
+function newQuote() {
+  const quote = apiQuote[Math.floor(Math.random() * apiQuote.length)];
+  // check if author field is blank and replace it with "Unknown"
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // Check quote length to determine styling
+  if (quote.text.length > 90) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
+}
+
+async function getQuotes() {
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuote = await response.json();
+    newQuote();
+  } catch (error) {
+    // Handle error here
+  }
+}
+
+// Tweet quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+// on page load
+getQuotes();
